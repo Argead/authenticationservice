9ea4ca5191559e4c392183b7c681bc0025fb20c0
Argead <michaelmackinnon@outlook.com>
2019-03-16 21:13:50 +0000

    converted existing code to basic class

--- key_generator.py
+++ key_generator.py
@@ -5,55 +5,55 @@ import hashlib
 import os
 import subprocess
 
-#Constants to define the size of the API secret key and the public identifier.
-API_KEY_LENGTH = 40
-ID_KEY_LENGTH = 20
-REQUIRED_MIN_ENTROPY = 100
+class APIKeyGenerator():
+    def __init__(self, api_key_length, id_key_length, req_min_entropy):
+        #Constants to define the size of the API secret key and the public identifier.
+        self.API_KEY_LENGTH = api_key_length
+        self.ID_KEY_LENGTH = id_key_length
+        self.REQUIRED_MIN_ENTROPY = req_min_entropy
 
-def create_key_pair():
-    #TODO: replace this magic numbers with configuration
-    #TODO: add exception handling
-    api_key = create_api_key(20)
-    identifier = create_api_key_identifier(10)
-    #Why return a tuple? why not JSONify the keys?
-    return (api_key, identifier)
+    #Generate a new API key. Bits is an int defining the number of random bits
+    # to request from the underlying source of entropy. 
+    def create_api_key(self, bits):
+        random_bits = os.urandom(bits)
+        #TODO: strategy pattern opportunity here when I convert this to OOP
+        # option of using different secure ciphers
+        sha = hashlib.sha256()
+        sha.update(random_bits)
+        sha_output = sha.hexdigest()
+        #Impose constrains on size of API key in case API_KEY_SIZE is modified
+        # What happens if this assert fails?
+        assert(self.API_KEY_LENGTH <= len(sha_output) and self.API_KEY_LENGTH > 10)
+        key = sha_output[:self.API_KEY_LENGTH]
+        #What happens if the function call fails before this return statement?
+        return key
 
-#Generate a new API key. Bits is an int defining the number of random bits
-# to request from the underlying source of entropy. 
-def create_api_key(bits):
-    random_bits = os.urandom(bits)
-    #TODO: strategy pattern opportunity here when I convert this to OOP
-    # option of using different secure ciphers
-    sha = hashlib.sha256()
-    sha.update(random_bits)
-    sha_output = sha.hexdigest()
-    #Impose constrains on size of API key in case API_KEY_SIZE is modified
-    # What happens if this assert fails?
-    assert(API_KEY_SIZE <= len(sha_output) and API_KEY_LENGTH > 10)
-    key = sha_output[:API_KEY_LENGTH]
-    #What happens if the function call fails before this return statement?
-    return key
+    #Change this approach => hash the api key and modify it? Rather than drawing on entropy pool more
+    def create_api_key_identifier(self, bits):
+        random_bits = os.urandom(bits)
+        #TODO: replace this implementation. Keep interface the same.
+        md = hashlib.md5()
+        md.update(random_bits)
+        output = md.hexdigest()[:self.ID_KEY_LENGTH]
+        key = output.upper()
+        return key
 
-#Change this approach => hash the api key and modify it? Rather than drawing on entropy pool more
-def create_api_key_identifier(bits):
-    random_bits = os.urandom(bits)
-    #TODO: replace this implementation. Keep interface the same.
-    md = hashlib.md5()
-    md.update(random_bits)
-    output = md.hexdigest()[:ID_KEY_LENGTH]
-    key = output.upper()
-    return key
+    def create_key_pair(self):
+        #TODO: replace this magic numbers with configuration
+        #TODO: add exception handling
+        api_key = create_api_key(20)
+        identifier = create_api_key_identifier(10)
+        #Why return a tuple? why not JSONify the keys?
+        return (api_key, identifier)
+
+    def entropy_health_check(self):
+        #This appraoch seems like a bad one - reading from kernel space. How else could the entropy pool's status be evaluated?
+        # How about moving to Python3.6 and using GRND_NONBLOCK flag?
     
-    
+        #Read from entropy_avail will only work on Linux
+        #TODO: Exception handling for the assertion
+        assert(os.uname().sysname == "Linux")
+        pool = subprocess.call(['cat', '/proc/sys/kernel/random/entropy_avail'], stdout=subprocess.PIPE)
+        available_entropy = int(pool.stdout)
+        return available_entropy >= self.REQUIRED_MIN_ENTROPY
 
-
-def entropy_health_check():
-    #This appraoch seems like a bad one - reading from kernel space. How else could the entropy pool's status be evaluated?
-    # How about moving to Python3.6 and using GRND_NONBLOCK flag?
-
-    #Read from entropy_avail will only work on Linux
-    #TODO: Exception handling for the assertion
-    assert(os.uname().sysname == "Linux")
-    pool = subprocess.call(['cat', '/proc/sys/kernel/random/entropy_avail'], stdout=subprocess.PIPE)
-    available_entropy = int(pool.stdout)
-    return available_entropy >= REQUIRED_MIN_ENTROPY
