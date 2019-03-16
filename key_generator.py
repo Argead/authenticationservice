8d768dab3cffa3d6c972d80d0a8611759ad5ef99
Argead <michaelmackinnon@outlook.com>
2019-03-16 21:42:05 +0000

    added exception handling to add_api_key method

--- key_generator.py
+++ key_generator.py
@@ -6,27 +6,48 @@ import os
 import subprocess
 
 class APIKeyGenerator():
-    def __init__(self, api_key_length, id_key_length, req_min_entropy):
+    def __init__(self, api_key_length, id_key_length, req_min_entropy, min_key_length):
         #Constants to define the size of the API secret key and the public identifier.
+        #TODO: clean up these constants
         self.API_KEY_LENGTH = api_key_length
         self.ID_KEY_LENGTH = id_key_length
         self.REQUIRED_MIN_ENTROPY = req_min_entropy
+        self.MIN_KEY_LENGTH = min_key_length
+        #Assume entropy pool is not ready by default. Only update with explicit check.
+        self.entropy_pool_ready = False
 
-    #Generate a new API key. Bits is an int defining the number of random bits
-    # to request from the underlying source of entropy. 
-    def create_api_key(self, bits):
-        random_bits = os.urandom(bits)
-        #TODO: strategy pattern opportunity here when I convert this to OOP
-        # option of using different secure ciphers
-        sha = hashlib.sha256()
-        sha.update(random_bits)
-        sha_output = sha.hexdigest()
-        #Impose constrains on size of API key in case API_KEY_SIZE is modified
-        # What happens if this assert fails?
-        assert(self.API_KEY_LENGTH <= len(sha_output) and self.API_KEY_LENGTH > 10)
-        key = sha_output[:self.API_KEY_LENGTH]
-        #What happens if the function call fails before this return statement?
-        return key
+    #Internal method used to create and return a new API key.
+    # random_bits is an int describing the number of bits to take from os.urandom()
+    # returns a tuple of (key, error). error is None if function call succeeds.
+    def _create_API_key(self, random_bits):
+        #TODO: clean up this method. Too long, too complex, too many return statements.
+        #      divide up into multiple functions
+        self._entropy_health_check()
+        if self.entropy_pool_ready:
+            bits = os.urandom(random_bits)
+            #TODO: develop more options than just SHA256. Strategy pattern (?)
+            sha = hashlib.sha256()
+            sha.update(bits)
+            sha_output = sha.hexdigest()
+
+            #Impose constraints on the size of the API key in case API_KEY_SIZE is modified
+            try:
+                assert(type(self.API_KEY_LENGTH == int))
+                assert(self.API_KEY_LENGTH <= len(sha_output) and self.API_KEY_LENGTH > self.MIN_KEY_LENGTH)
+                key = sha_output[:self.API_KEY_LENGTH]
+                return (key, None)
+            except AssertionError:
+                error = Exception("API Key Length parameters are invalid.")
+                return ('', error)
+        else:
+            error = Exception("The entropy pool is not ready")
+            return ('', error)
+
+
+
+
+
+
 
     #Change this approach => hash the api key and modify it? Rather than drawing on entropy pool more
     def create_api_key_identifier(self, bits):
