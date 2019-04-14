cbc28d237957473d691c1b70d41631657eed7604
Argead <michaelmackinnon@outlook.com>
2019-04-14 02:05:00 +0000

    Created basic implementation of KeyValidator class.

--- services/authentication/key_validator.py
+++ services/authentication/key_validator.py
@@ -7,11 +7,6 @@ class KeyValidatorABC(ABC):
     """Base class for KeyValidator implementations."""
 
     @abstractmethod
-    def __init__(self, cipher, key_length):
-        """Provide cipher type to validate against."""
-        return
-
-    @abstractmethod
     def validate_api_key(self, api_key):
         """Validate provided api key for well-formedness."""
         return
@@ -20,3 +15,25 @@ class KeyValidatorABC(ABC):
     def create_request_signature(self, api_key, request):
         """For more complex signature scheme, resign request."""
         return
+
+
+class BasicKeyValidator(KeyValidatorABC):
+    """Most basic KeyValidator implementation. Only checks key length."""
+
+    def __init__(self, cipher, key_length):
+        """
+        Cipher not strictly necessary for this basic implementation.
+
+        Parameter cipher - string name of cipher used to generate key.
+        Parameter key_length - int length of key length for cipher..
+        """
+        self.cipher = cipher
+        self.key_length = key_length
+
+    def validate_api_key(self, api_key):
+        """Check the length of provided api key."""
+        return len(api_key) == self.key_length
+
+    def create_request_signature(self, api_key, request):
+        """Validate key sig. Method is not needed for basic validator."""
+        raise NotImplementedError
