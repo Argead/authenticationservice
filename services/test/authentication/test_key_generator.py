f6467ad12e42e0477879b656dec9edd9e5bd890c
Argead <michaelmackinnon@outlook.com>
2019-04-15 23:19:26 +0000

    added basic tests for key generator class.

--- services/test/authentication/test_key_generator.py
+++ services/test/authentication/test_key_generator.py
@@ -2,15 +2,30 @@
 """Test suite for the KeyGenerator classes."""
 
 import unittest
-from .services.authentication import BasicKeyGenerator, BasicKeyValidator
+from ...authentication.key_generator import BasicKeyGenerator, _KeyGeneratorABC
 
 class TestBasicKeyGenerator(unittest.TestCase):
     """Test suite for BasicKeyGenerator class."""
 
     def test_BasicKeyGenerator_exists(self):
+        """Test if the BasicKeyGenerator can be created."""
         generator = BasicKeyGenerator()
-        print(type(generator))
-        self.assertTrue(False)
+        self.assertIsInstance(generator, BasicKeyGenerator)
+        self.assertIsInstance(generator, _KeyGeneratorABC)
+
+    def test_BasicKeyGenerator_can_create_api_keys(self):
+        """Test if the generate_api_key method produces output."""
+        generator = BasicKeyGenerator()
+        new_key = ''
+        new_key = generator.generate_api_key()
+        self.assertIsNot(new_key, '')
+
+    def test_BasicKeyGenerator_creates_well_formed_api_keys(self):
+        """Test if the generate_api_key method produces right output."""
+        generator = BasicKeyGenerator()
+        new_key = ''
+        new_key = generator.generate_api_key()
+        self.assertEqual(len(new_key), generator.key_length)
 
 
 if __name__ == "__main__":
